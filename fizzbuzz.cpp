74da6af18f459d0929ae8c9525a091df7b4fe2ab
Augusto Samussone <augustooalcides@gmail.com>
2022-07-14 18:13:52 +0100

    First commit

---
+++ fizzbuzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 1; i<= 100; i++){
+        if(i%3==0 && i%5==0){
+                cout << "FizzBuzz";
+        }else if(i % 5 == 0){
+                cout << "buzz";
+        }else if(i%3==0)
+            cout<<"Fizz";
+        else {
+            cout << i;
+        }
+    }
+    return 0;
+}
